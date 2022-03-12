8301df75f2984b06c10f9581a15e6377f673de24
Samantha Lindsey <sammarielindsey@gmail.com>
2022-03-12 16:02:53 +1300

    Game Manager

---
+++ Desktop/polytech/2022/BIT/TowerDefence/starter-code/Assets/Scripts/GameManagerBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerBehaviour : MonoBehaviour
+{
+    public Text goldLabel;
+    private int gold;
+    public int Gold 
+        {   
+                get { return gold; }
+                set
+            {
+                gold = value;
+                goldLabel.GetComponent<Text>().text = "GOLD: " + gold;
+            }
+  
+        }
+
+
+    void Start()
+    {
+        Gold = 1000;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
