b7ca5a74f856c9c41ea88c36ed90c1e4cfb7ed82
Samantha Lindsey <sammarielindsey@gmail.com>
2022-03-13 13:32:00 +1300

    Managing Enemies

---
+++ Desktop/polytech/2022/BIT/TowerDefence/starter-code/Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+
+    [HideInInspector]
+
+    public GameObject[] waypoints;
+    private int currentWaypoint = 0;
+    private float lastWaypointSwitchTime;
+    public float speed = 1.0f;
+
+
+    void Start()
+    {
+        lastWaypointSwitchTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 startPosition = waypoints [currentWaypoint].transform.position;
+        Vector3 endPosition = waypoints [currentWaypoint + 1].transform.position;
+
+        float pathLength = Vector3.Distance (startPosition, endPosition);
+        float totalTimeForPath = pathLength / speed;
+        float currentTimeOnPath = Time.time - lastWaypointSwitchTime;
+        gameObject.transform.position = Vector2.Lerp (startPosition, endPosition, currentTimeOnPath / totalTimeForPath);
+
+        if (gameObject.transform.position.Equals(endPosition)) 
+            {
+                if (currentWaypoint < waypoints.Length - 2)
+                    {
+                        currentWaypoint++;
+                        lastWaypointSwitchTime = Time.time;
+                        // TODO: Rotate into move direction
+                    }
+                else
+                    {
+                        Destroy(gameObject);
+
+                        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+                        AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
+                        // TODO: deduct health
+                    }
+            }
+    }
+}
---
+++ Desktop/polytech/2022/BIT/TowerDefence/starter-code/Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+
+    public GameObject[] waypoints;
+    public GameObject testEnemyPrefab;
+
+
+    void Start()
+    {
+        Instantiate(testEnemyPrefab).GetComponent<MoveEnemy>().waypoints = waypoints;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
