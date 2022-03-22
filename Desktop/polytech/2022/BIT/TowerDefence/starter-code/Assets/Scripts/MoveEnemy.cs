7ce73622bc982c60a3895697c85969a0322fb0f5
Samantha Lindsey <sammarielindsey@gmail.com>
2022-03-23 08:19:05 +1300

    Rotating Enemy

--- Desktop/polytech/2022/BIT/TowerDefence/starter-code/Assets/Scripts/MoveEnemy.cs
+++ Desktop/polytech/2022/BIT/TowerDefence/starter-code/Assets/Scripts/MoveEnemy.cs
@@ -35,7 +35,7 @@ public class MoveEnemy : MonoBehaviour
                     {
                         currentWaypoint++;
                         lastWaypointSwitchTime = Time.time;
-                        // TODO: Rotate into move direction
+                        RotateIntoMoveDirection();
                     }
                 else
                     {
@@ -47,4 +47,20 @@ public class MoveEnemy : MonoBehaviour
                     }
             }
     }
+
+    private void RotateIntoMoveDirection()
+        {
+            Vector3 newStartPosition = waypoints [currentWaypoint].transform.position;
+            Vector3 newEndPosition = waypoints [currentWaypoint + 1].transform.position;
+            Vector3 newDirection = (newEndPosition - newStartPosition);
+
+            float x = newDirection.x;
+            float y = newDirection.y;
+            float rotationAngle = Mathf.Atan2 (y, x) * 180 / Mathf.PI;
+
+            GameObject sprite = gameObject.transform.Find("Sprite").gameObject;
+            sprite.transform.rotation = Quaternion.AngleAxis(rotationAngle, Vector3.forward);
+        }
+
+
 }
