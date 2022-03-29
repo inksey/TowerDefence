0bb48f0d0e06f3051b61e88c90ff8ec779c9861a
Samantha Lindsey <sammarielindsey@gmail.com>
2022-03-29 22:21:26 +1300

    Target Switching

--- Desktop/polytech/2022/BIT/TowerDefence/starter-code/Assets/Scripts/MoveEnemy.cs
+++ Desktop/polytech/2022/BIT/TowerDefence/starter-code/Assets/Scripts/MoveEnemy.cs
@@ -12,6 +12,18 @@ public class MoveEnemy : MonoBehaviour
     private float lastWaypointSwitchTime;
     public float speed = 1.0f;
 
+    public float DistanceToGoal()
+    {
+        float distance = Vector2.Distance(gameObject.transform.position, waypoints[currentWaypoint + 1].transform.position);
+        for (int i = currentWaypoint + 1; i < waypoints.Length - 1; i++)
+    {
+        Vector3 startPosition = waypoints[i].transform.position;
+        Vector3 endPosition = waypoints[i + 1].transform.position;
+        distance += Vector2.Distance(startPosition, endPosition);
+    }
+        return distance;
+    }
+    
 
     void Start()
     {
